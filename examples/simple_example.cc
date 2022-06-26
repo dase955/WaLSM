f36f5d44998899eeece88e537adc2c48fc522ab8
Chen Lixiang <chenlixiang.dev@gmail.com>
2022-06-26 15:31:39 +0800

    add benchmark as simple_example

--- examples/simple_example.cc
+++ examples/simple_example.cc
@@ -1,38 +1,204 @@
-// Copyright (c) 2011-present, Facebook, Inc.  All rights reserved.
-//  This source code is licensed under both the GPLv2 (found in the
-//  COPYING file in the root directory) and Apache 2.0 License
-//  (found in the LICENSE.Apache file in the root directory).
-
-#include <cstdio>
-#include <string>
-#include <random>
-#include <vector>
-#include <unordered_set>
 #include <algorithm>
+#include <cstdio>
 #include <iostream>
+#include <random>
+#include <string>
+#include <thread>
+#include <vector>
 
 #include "rocksdb/db.h"
-#include "rocksdb/slice.h"
 #include "rocksdb/options.h"
+#include "rocksdb/slice.h"
 
 using namespace ROCKSDB_NAMESPACE;
 
-std::string kDBPath = "/Users/chenlixiang/rocksdb_test";
+const std::string kDBPath = "/Users/chenlixiang/rocksdb_test";
+const size_t kInsert = 1000000;
+const size_t kOps = 500000;
+const size_t insert_rate = 3;
+const size_t query_rate = 7;
 
-std::string randomString(int len) {
-  char tmp;
-  std::string buf;
+const size_t kPrintGap = 100000;
 
-  std::random_device dev;
-  std::default_random_engine random(dev());
+const int hot_range = 1;  // 1/10
+const int hot_rate = 9;   // 9/10
 
-  for (int i = 0; i < len; i++) {
-    tmp = random() % 36;
-    tmp += (tmp < 10) ? '0' : ('a'-10);
-    buf += tmp;
+const double hot_prob = static_cast<double>(hot_rate) / 10.0;
+
+const size_t op_insert_num = (insert_rate * kOps) / (insert_rate + query_rate);
+const size_t op_query_num = (query_rate * kOps) / (insert_rate + query_rate);
+
+const uint64_t hot_insert_up_bound = UINT64_MAX;
+const uint64_t hot_insert_low_bound =
+    (10 - static_cast<uint64_t>(hot_range)) * (hot_insert_up_bound / 10);
+const uint64_t hot_read_low_bound = 0;
+const uint64_t hot_read_up_bound =
+    hot_read_low_bound + static_cast<uint64_t>(hot_range) * (UINT64_MAX / 10);
+
+const int t_num = 8;
+
+struct TestContext {
+  std::atomic<size_t> inserted;
+  std::atomic<size_t> run_insert;
+  std::atomic<size_t> run_query;
+  std::atomic<size_t> total_ops;
+
+  uint64_t* insert_nums;
+  uint64_t* op_insert;
+  uint64_t* op_query;
+
+  TestContext() : inserted(0), run_insert(0), run_query(0), total_ops(0) {
+    insert_nums = new uint64_t[kInsert];
+    op_insert = new uint64_t[op_insert_num];
+    op_query = new uint64_t[op_query_num];
   }
 
-  return buf;
+  ~TestContext() {
+    delete[] insert_nums;
+    delete[] op_insert;
+    delete[] op_query;
+  }
+};
+
+uint64_t systemTime() {
+  uint64_t ms = std::chrono::duration_cast<std::chrono::milliseconds>(
+                    std::chrono::system_clock::now().time_since_epoch())
+                    .count();
+  return ms;
+}
+
+bool randomBool(double prob) {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<> dis(0, 1);
+  double rn = dis(gen);
+  return rn < prob;
+}
+
+uint64_t randomUINT64T(uint64_t a, uint64_t b) {
+  std::random_device rd;
+  std::mt19937_64 gen(rd());
+  std::uniform_int_distribution<uint64_t> dis(a, b);
+  return dis(gen);
+}
+
+size_t randomSIZET(size_t a, size_t b) {
+  std::random_device rd;
+  std::mt19937_64 gen(rd());
+  std::uniform_int_distribution<size_t> dis(a, b);
+  return dis(gen);
+}
+
+inline std::string numToKey(uint64_t num) {
+  std::string num_str = std::to_string(num);
+  while (num_str.size() < 20) num_str = '0' + num_str;
+  return "user_data_" + num_str;
+}
+
+inline std::string numToValue(uint64_t num) {
+  std::string value = numToKey(num);
+  value.append(1024 - value.size(), 'x');
+  return value;
+}
+
+void genData(TestContext* ctx) {
+  std::vector<uint64_t> hot_read_keys;
+  // generate op insert data
+  for (size_t i = 0; i < op_insert_num; i++) {
+    if (randomBool(hot_prob)) {
+      ctx->op_insert[i] =
+          randomUINT64T(hot_insert_low_bound, hot_insert_up_bound);
+    } else {
+      uint64_t num = randomUINT64T(0, hot_insert_low_bound);
+      ctx->op_insert[i] = num;
+      if (num >= hot_read_low_bound && num <= hot_insert_up_bound) {
+        hot_read_keys.push_back(ctx->op_insert[i]);
+      }
+    }
+  }
+
+  // generate init data
+  for (size_t i = 0; i < kInsert; i++) {
+    uint64_t num = randomUINT64T(0, UINT64_MAX);
+    ctx->insert_nums[i] = num;
+    if (num >= hot_read_low_bound && num <= hot_read_up_bound) {
+      hot_read_keys.push_back(num);
+    }
+  }
+
+  // generate op query data
+  for (size_t i = 0; i < op_query_num; i++) {
+    if (randomBool(hot_prob)) {
+      size_t idx = randomSIZET(0, hot_read_keys.size() - 1);
+      ctx->op_query[i] = hot_read_keys[idx];
+    } else {
+      size_t idx = randomSIZET(0, op_insert_num - 1);
+      ctx->op_query[i] = ctx->op_insert[idx];
+    }
+  }
+}
+
+void insertData(DB* db, TestContext* ctx, uint64_t start) {
+  Status s;
+  size_t idx;
+  auto options = WriteOptions();
+  std::string key, value;
+  while ((idx = ctx->inserted.fetch_add(1)) < kInsert) {
+    uint64_t num = ctx->insert_nums[idx];
+    key = numToKey(num), value = numToValue(num);
+    s = db->Put(options, key, value);
+    assert(s.ok());
+    if ((idx + 1) % kPrintGap == 0) {
+      uint64_t delta = systemTime() - start;
+      std::cout << idx + 1 << " inserts take " << delta / 1000 << "."
+                << delta % 1000 << "s" << std::endl;
+    }
+  }
+}
+
+void runOps(DB* db, TestContext* ctx, uint64_t start) {
+  Status s;
+  size_t idx;
+  auto read_options = ReadOptions();
+  auto write_options = WriteOptions();
+  std::string key, value;
+  uint64_t num;
+  for (;;) {
+    for (size_t i = 0; i < insert_rate; i++) {
+      idx = ctx->run_insert.fetch_add(1);
+      if (idx >= op_insert_num) {
+        break;
+      }
+      num = ctx->op_insert[idx];
+      key = numToKey(num), value = numToValue(num);
+      s = db->Put(write_options, key, value);
+      assert(s.ok());
+    }
+
+    for (size_t i = 0; i < query_rate; i++) {
+      idx = ctx->run_insert.fetch_add(1);
+      if (idx >= op_query_num) {
+        break;
+      }
+      num = ctx->op_query[idx];
+      key = numToKey(num);
+      s = db->Get(read_options, key, &value);
+      if (s.ok()) {
+        assert(value == numToValue(num));
+      }
+      break;
+    }
+
+    size_t cur_ops = ctx->total_ops.fetch_add(insert_rate + query_rate);
+    if ((cur_ops % kPrintGap) == 0) {
+      uint64_t delta = systemTime() - start;
+      std::cout << cur_ops << " operations take " << delta / 1000 << "."
+                << delta % 1000 << " s" << std::endl;
+    }
+    if (cur_ops >= kOps) {
+      break;
+    }
+  }
 }
 
 int main() {
@@ -42,118 +208,41 @@ int main() {
   options.IncreaseParallelism();
   options.OptimizeUniversalStyleCompaction();
   options.use_direct_io_for_flush_and_compaction = true;
+  options.use_direct_reads = true;
   // create the DB if it's not already present
   options.create_if_missing = true;
-//  options.target_file_size_base = 4 * 1048576;
   options.write_buffer_size = 4 << 20;
+  options.max_bytes_for_level_base = 64 << 20;
   options.level0_file_num_compaction_trigger = 4;
-  options.use_direct_io_for_flush_and_compaction = true;
-  options.use_direct_reads = true;
-
-
+  options.max_background_jobs = 8;
+  options.max_background_compactions = 8;
 
   // open DB
   Status s = DB::Open(options, kDBPath, &db);
   assert(s.ok());
 
-  std::cout << " db initialized " << std::endl;
+  // init test context
+  std::cout << "start generating data..." << std::endl;
+  TestContext ctx;
+  genData(&ctx);
+  std::cout << "generating data ok!" << std::endl;
 
-  db->Put(WriteOptions(), "key1", "value1");
-
-  // Put key-value
-//  s = db->Put(WriteOptions(), "key1", "value");
-//  assert(s.ok());
-  std::string value;
-  // get value
-//  s = db->Get(ReadOptions(), "key1", &value);
-
-  std::unordered_map<std::string, std::string> kvs;
-  std::unordered_set<std::string> repeat_keys;
-  for (int i = 0; i < 10000000; i++) {
-    std::string my_key = randomString(128);
-    std::string my_value = randomString(200);
-    if (kvs.find(my_key) != kvs.end()) {
-      repeat_keys.insert(my_key);
-      kvs[my_key] = my_value;
-    } else if (i % 30 == 1) {
-      kvs[my_key] = my_value;
-    }
-    db->Put(WriteOptions(), my_key, my_value);
+  std::thread ts[t_num];
+  for (int i = 0; i < t_num; i++) {
+    ts[i] = std::thread(insertData, db, &ctx, systemTime());
+  }
+  for (int i = 0; i < t_num; i++) {
+    ts[i].join();
   }
 
-  s = db->Get(ReadOptions(), "key1", &value);
-  assert(s.ok());
-  assert(value == "value1");
+  std::cout << "************************" << std::endl;
 
-  std::cout << " insert finished." << std::endl;
-
-  for (int i = 0; i < 100; i++) {
-    // test random get
-    db->Get(ReadOptions(), randomString(64), &value);
+  for (int i = 0; i < t_num; i++) {
+    ts[i] = std::thread(runOps, db, &ctx, systemTime());
   }
-
-  // check data
-  int no_hit = 0, repeat = 0, bad_value = 0;
-  for (auto& kv : kvs) {
-    s = db->Get(ReadOptions(), kv.first, &value);
-    if (s.IsNotFound()) {
-      no_hit++;
-    }
-    else if (value != kv.second) {
-      if (repeat_keys.find(kv.first) != repeat_keys.end()) {
-        std::cout << "REPEAT KEY ERROR!" << "$";
-        repeat++;
-      }
-      std::cout << "key: " << kv.first << " expect: " << kv.second
-      << " got: " << value << std::endl;
-      bad_value++;
-    }
+  for (int i = 0; i < t_num; i++) {
+    ts[i].join();
   }
-  printf("%d/%d not_found. %d/%d repeat/bad values. total repeat data size %d", no_hit, (int) kvs.size(),
-         repeat, bad_value, (int)repeat_keys.size());
-
-  // atomically apply a set of updates
-  {
-    WriteBatch batch;
-    batch.Delete("key1");
-    batch.Put("key2", "value");
-    s = db->Write(WriteOptions(), &batch);
-  }
-
-  s = db->Get(ReadOptions(), "key1", &value);
-  assert(s.IsNotFound());
-
-  db->Get(ReadOptions(), "key2", &value);
-  assert(value == "value");
-
-  {
-    PinnableSlice pinnable_val;
-    db->Get(ReadOptions(), db->DefaultColumnFamily(), "key2", &pinnable_val);
-    assert(pinnable_val == "value");
-  }
-
-  {
-    std::string string_val;
-    // If it cannot pin the value, it copies the value to its internal buffer.
-    // The intenral buffer could be set during construction.
-    PinnableSlice pinnable_val(&string_val);
-    db->Get(ReadOptions(), db->DefaultColumnFamily(), "key2", &pinnable_val);
-    assert(pinnable_val == "value");
-    // If the value is not pinned, the internal buffer must have the value.
-    assert(pinnable_val.IsPinned() || string_val == "value");
-  }
-
-  PinnableSlice pinnable_val;
-  s = db->Get(ReadOptions(), db->DefaultColumnFamily(), "key1", &pinnable_val);
-  assert(s.IsNotFound());
-  // Reset PinnableSlice after each use and before each reuse
-  pinnable_val.Reset();
-  db->Get(ReadOptions(), db->DefaultColumnFamily(), "key2", &pinnable_val);
-  assert(pinnable_val == "value");
-  pinnable_val.Reset();
-  // The Slice pointed by pinnable_val is not valid after this point
-
-  delete db;
 
   return 0;
 }
